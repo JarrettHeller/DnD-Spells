81e20f633a5ebed542d81f32e16572e34d9c22e3
Lucas Plumb <Lucasplumb@live.com>
2021-01-23 12:57:25 -0600

    did some script work, put in ajax

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-var queryURL = `https://www.dnd5eapi.co/api/classes/${class}/spells`
 
-console.log(queryURL)
+$(".character li").on("click", function(){
+    var DnDclass = $(this).text()
+
+    console.log(DnDclass)
+
+    displaySpell(DnDclass);
+
+})
+
+function displaySpell(DnDClass){
+    var queryURL = `https://www.dnd5eapi.co/api/classes/${DnDclass}/spells`
+
+$.ajax({
+    type: "GET",
+    URL: queryURL
+}).then(function(response){
+
+    console.log(response)
+    
+    
+})
+}
